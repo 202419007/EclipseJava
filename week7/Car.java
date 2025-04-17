7b495bb0e5a6a9f5133d1dc0dea5e381a82537f1
202419007 <202419007@itc.ac.kr>
2025-04-17 11:35:56 +0900

    Update Car.java

--- week7/Car.java
+++ week7/Car.java
@@ -9,9 +9,24 @@ public class Car {
 	int speed;
 	
 	// 생성자가 없으면 컴파일러가 기본 생성자를 자동으로 만들어줌
-	// 생성자가 하나라도 정의되어 있으면 기본 생성자는 안만들어
+	// 생성자가 하나라도 정의되어 있으면 기본 생성자는 안만들어짐
+	
+	// 생성자가 여러 개 선언 => 생성자 오버로딩(overloading)
+	// 초기화를 다양하게 할 수 있도록 제공
 	Car() {
 		// 기본 생성
+	}
+	
+	Car(String model) {
+		this.model = model;
+		this.color = "black";
+		this.maxSpeed = 300;
+	}
+	
+	Car(String model, String color) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = 250;
 	}
 	
 	// 매개변수가 있는 생성자 정의
