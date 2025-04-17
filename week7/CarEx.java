4198d3aaf343c329018cb6911cd904e44c264c6e
202419007 <202419007@itc.ac.kr>
2025-04-17 11:07:11 +0900

    Update CarEx.java

--- week7/CarEx.java
+++ week7/CarEx.java
@@ -4,6 +4,7 @@ public class CarEx {
 
 	public static void main(String[] args) {
 		// 객체 생성
+		// Car() : 생성자
 		Car mycar = new Car();
 		
 		// 필드 값 읽기
@@ -25,6 +26,21 @@ public class CarEx {
 		System.out.println("색 상:"+yourcar.color);
 		System.out.println("최고속도:"+yourcar.maxSpeed);
 		System.out.println("현재속도:"+yourcar.speed);
+		
+		// 매개변수를 이용한 객체 생성
+		Car mycar2 = new Car("그랜저", "white", 300);
+		System.out.println("제조회사:"+mycar2.company);
+		System.out.println("모델명:"+mycar2.model);
+		System.out.println("색 상:"+mycar2.color);
+		System.out.println("최고속도:"+mycar2.maxSpeed);
+		System.out.println("현재속도:"+mycar2.speed);
+		
+		Car yourcar2 = new Car("소나타", "black", 250);
+		System.out.println("제조회사:"+yourcar2.company);
+		System.out.println("모델명:"+yourcar2.model);
+		System.out.println("색 상:"+yourcar2.color);
+		System.out.println("최고속도:"+yourcar2.maxSpeed);
+		System.out.println("현재속도:"+yourcar2.speed);
 	}
 
 }
