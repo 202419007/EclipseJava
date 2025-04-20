c82273d0de6375ef1740c956cd850b3cd8a2d55e
202419007 <202419007@itc.ac.kr>
2025-04-20 23:30:51 +0900

    Update Print.java

--- week2/Print.java
+++ week2/Print.java
@@ -8,7 +8,7 @@ public class Print {
 		//실행 단축키 : ctrl+F11
 		System.out.println("객체지향 프로그래밍");
 	
-		//주석 단축키 : 
+		//주석 단축키 : ctrl + /
 		System.out.println("/*객체지향*/ 프로그래밍");
 	
 		//숫자 출력 시 주의사항
@@ -22,9 +22,18 @@ public class Print {
 		//%d : 정수 출력
 		//%f : 실수 출력
 		//\n : 줄바꿈
-		System.out.printf("이름 : %s", "홍길동");
+		System.out.printf("이름 : %s\n", "홍길동");
 		System.out.printf("나이 : %d세\n", 25);
 		
 		System.out.printf("과목 : %s%s %s\n", "객체","지향", "프로그래밍");
 	}
 }
+
+// 출력
+// 객체지향 프로그래밍
+// /*객체지향*/ 프로그래밍
+// 52
+// 17 + 35 = 52
+// 이름 : 홍길동
+// 나이 : 25세
+// 과목 : 객체지향 프로그래밍
