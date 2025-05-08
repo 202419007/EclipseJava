1c75312b2c2cdca9fa73b39e5ac3f19f8911fc06
202419007 <202419007@itc.ac.kr>
2025-05-08 11:33:11 +0900

    Create SmartPhone.java

---
+++ week10/SmartPhone.java
@@ -0,0 +1,26 @@
+package week10;
+
+public class SmartPhone extends Phone{
+	// 부모 클래스 Phon을 상속 받음
+	private boolean wifi;
+	
+	public SmartPhone(String model, String color) {
+		this.model = model;
+		this.color = color;
+		
+		System.out.println("SmartPhone() 생성자 호출");
+	}
+	
+	public boolean isWifi() {
+		return wifi;
+	}
+
+	public void setWifi(boolean wifi) {
+		this.wifi = wifi;
+		System.out.println("wifi 상태 변경");
+	}
+	
+	public void internet() {
+		System.out.println("인터넷 연결");
+	}
+}
