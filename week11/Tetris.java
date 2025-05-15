4aab3254b7f5ccdca65558ee01656532cb12c1fd
202419007 <202419007@itc.ac.kr>
2025-05-15 12:13:57 +0900

    Update Tetris.java

--- week11/Tetris.java
+++ week11/Tetris.java
@@ -8,7 +8,7 @@ public class Tetris extends Game {
 
 	@Override
 	public void gameDesc() {
-		System.out.println("Tetris는 퍼즐 게임으로, 소련의 개발자가 처음 디자인하고 프로그래밍한 게임이다.");
+		System.out.println("테트리스는 퍼즐 게임으로, 소련의 개발자가 처음 디자인하고 프로그래밍한 게임이다.");
 	}
 
 }
