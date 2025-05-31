0b93f32a6e55d38d0717148c6e9499d849841349
202419007 <202419007@itc.ac.kr>
2025-05-31 18:41:21 +0900

    Update ProducerThread.java

--- week13/ProducerThread.java
+++ week13/ProducerThread.java
@@ -12,7 +12,7 @@ public class ProducerThread extends Thread {
 	public void run() {
 		// 생산자 작업 내용
 		// data를 쓰기만 하는 직업
-		for (int i=1; i<3; i++) {
+		for (int i=1; i<=3; i++) {
 			String data = "Data-" + i;
 			try {
 				dataBox.setData(data);
