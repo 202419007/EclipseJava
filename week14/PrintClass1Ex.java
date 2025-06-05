835ba47617de6eeee223991813ff4e79b58ab66a
202419007 <202419007@itc.ac.kr>
2025-06-05 10:12:39 +0900

    Create PrintClass1Ex.java

---
+++ week14/PrintClass1Ex.java
@@ -0,0 +1,17 @@
+package week14;
+
+public class PrintClass1Ex {
+
+	public static void main(String[] args) {
+		PrintClass1 pc1 = new PrintClass1();
+		pc1.printVal(100);
+		pc1.printVal("홍길동");
+		pc1.printVal(true);
+	}
+
+}
+
+// 출력
+// value = 100
+// value = 홍길동
+// value = true
