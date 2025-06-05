5ca5d16d3a090cc5bcb959e7b5a695e0e4e37165
202419007 <202419007@itc.ac.kr>
2025-06-05 10:31:26 +0900

    Create BoxEx.java

---
+++ week14/BoxEx.java
@@ -0,0 +1,17 @@
+package week14;
+
+public class BoxEx {
+
+	public static void main(String[] args) {
+		Box box = new Box();
+		box.setObj("Hello");
+		System.out.println(box.getObj());
+		String sval = (String)box.getObj();
+		
+		box.setObj(100);
+		int ival = (int)box.getObj();
+	}
+
+}
+
+// 출력 : Hello
