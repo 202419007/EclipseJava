bffea279a1ab9ec76e8b065678b96862a0fb006b
202419007 <202419007@itc.ac.kr>
2025-06-05 10:29:09 +0900

    Create Box.java

---
+++ week14/Box.java
@@ -0,0 +1,14 @@
+package week14;
+
+public class Box {
+	// 모든  타입의 데이터 저장 가능
+	private Object obj;
+
+	public Object getObj() {
+		return obj;
+	}
+
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+}
